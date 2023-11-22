e8ddec294e8e6e7be89147ef32d31272cc807628
Alexis Julien <aju@Alexiss-MacBook-Pro.local>
2023-11-22 13:00:30 +0900

    initialize Query Client provider in App root

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,14 @@
 import styles from "./app.module.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const App = (): JSX.Element => {
-  return <main className={styles.main}></main>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <main className={styles.main}></main>
+    </QueryClientProvider>
+  );
 };
 
 export default App;
